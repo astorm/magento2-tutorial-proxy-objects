d020a38d3772bc7d56053eeb8deb1d79ff187bc8
Amit <amit103065@gmail.com>
2016-02-05 18:46:05 +0530

    functionality to check whether Pulsestorm_TutorialProxy1 module is enabled or not

--- app/code/Pulsestorm/TutorialProxy2/Command/Testbed.php
+++ app/code/Pulsestorm/TutorialProxy2/Command/Testbed.php
@@ -5,13 +5,15 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use \Magento\Framework\ObjectManagerInterface;
+use \Magento\Framework\Module\Manager;
 
 class Testbed extends Command
 {
     protected $objectManager;
-    public function __construct(ObjectManagerInterface $manager)
+    public function __construct(ObjectManagerInterface $manager, Manager $moduleManager)
     {
         $this->objectManager = $manager;
+        $this->moduleManager = $moduleManager;
         parent::__construct();
     }
 
@@ -19,7 +21,7 @@ class Testbed extends Command
     {
         return $this->objectManager;
     }
-    
+
     protected function configure()
     {
         $this->setName("ps:tutorial-proxy");
@@ -30,24 +32,24 @@ class Testbed extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->installedCheck($output);
-        // $service = $this->createService($output);        
+        // $service = $this->createService($output);
         // $this->sayHelloWithFastObject($service, $output);
         // $this->sayHelloWithSlowObject($service, $output);
     }
-    
+
     protected function installedCheck($output)
     {
-        $output->writeln("You've installed Pulsestorm_TutorialProxy2!");          
-        if(true)
+        $output->writeln("You've installed Pulsestorm_TutorialProxy2!");
+        if($this->moduleEnableCheck())
         {
-            $output->writeln("You've also installed Pulsestorm_TutorialProxy1!");          
+            $output->writeln("You've also installed Pulsestorm_TutorialProxy1!");
         }
         else
         {
-            $output->writeln("Error: could not find Pulsestorm_TutorialProxy1!");                  
+            $output->writeln("Error: could not find Pulsestorm_TutorialProxy1!");
         }
     }
-    
+
     protected function sayHelloWithFastObject($service, $output)
     {
         $output->writeln("About to say hello with fast object");
@@ -57,17 +59,17 @@ class Testbed extends Command
         $output->writeln("Said hello with fast object, approximate time to load: " . round(($to_run*1000),4) . ' ms');
         $output->writeln('');
     }
-    
+
     protected function sayHelloWithSlowObject($service, $output)
     {
         $time = microtime(true);
-        $output->writeln("About to say hello with slow object");    
+        $output->writeln("About to say hello with slow object");
         $service->sayHelloWithSlowObject();
         $to_run = microtime(true) - $time;
         $output->writeln("Said hello with slow object, approximate time to load: " . round(($to_run * 1000),4) . ' ms');
         $output->writeln('');
     }
-    
+
     protected function createService($output)
     {
         $output->writeln("About to Create Service");
@@ -75,8 +77,11 @@ class Testbed extends Command
         $time = microtime(true);
         $service = $om->get('Pulsestorm\TutorialProxy1\Model\Example');
         $to_load = microtime(true) - $time;
-        $output->writeln("Created Service, approximate time to load: " . round(($to_load*1000),4) . ' ms');        
+        $output->writeln("Created Service, approximate time to load: " . round(($to_load*1000),4) . ' ms');
         $output->writeln('');
-        return $service;    
+        return $service;
+    }
+    protected function moduleEnableCheck() {
+        return $this->moduleManager->isEnabled('Pulsestorm_TutorialProxy1');
     }
 } 
